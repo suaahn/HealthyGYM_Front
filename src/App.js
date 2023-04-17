5b4d2d8b4293b2d94bcedc23799eb967d9988564
admin <lticdiyk@gmail.com>
2023-04-17 14:45:35 +0900

    commit_kyk_04/17_14:45

--- src/App.js
+++ src/App.js
@@ -11,106 +11,113 @@ import Login from "./components/login";
 import Regi from "./components/regi";
 import Bbsanswer from "./components/Bbsanswer";
 import Bbsupdate from "./components/Bbsupdate";
+import Mypage from "./components/Mypage";
 
 import Multicampus from "./asset/multicampus.png";
 
 function App() {
   return (
-    <div>
-      <header className="py-4">
-        <div className="container text-center">
-          <img alt="" src={Multicampus} width="300"/>
-        </div>
-      </header>
-
-      <BrowserRouter>
-
-        <nav className="navbar navbar-expand-md navbar-dark bg-info sticky-top">
-          <div className="container">
-
-            <div className="collapse navbar-collapse" id="navbar-content">
-              <ul className="navbar-nav mr-auto">
-
-                <li className="nav-item">
-                  <Link className="nav-link" to="/">Home</Link>
-                </li>
-
-                <li className="nav-item">
-                  <Link className="nav-link" to="/bbslist">게시판</Link>
-                </li>
-
-                <li className="nav-item">
-                  <Link className="nav-link" to="/pdslist">자료실</Link>
-                </li>   
-
-                <li className="nav-item">
-                  <Link className="nav-link" to="/write">write</Link>
-                </li> 
-
-                <li className="nav-item">
-                  <Link className="nav-link" to="/viewer/1">viewer</Link>
-                </li> 
-
-              </ul>
-            </div>
+      <div>
+        <header className="py-4">
+          <div className="container text-center">
+            <img alt="" src={Multicampus} width="300"/>
           </div>
-        </nav>
+        </header>
 
-        <main>
-          <div className="py-4">
+        <BrowserRouter>
+
+          <nav className="navbar navbar-expand-md navbar-dark bg-info sticky-top">
             <div className="container">
-              <Routes>
-                <Route path="/" element={<Home />}></Route>
 
-                <Route path="/bbslist" element={<Bbslist />}></Route>
-                <Route path="/bbslist/:choice/:search" element={<Bbslist />}></Route>
+              <div className="collapse navbar-collapse" id="navbar-content">
+                <ul className="navbar-nav mr-auto">
 
-                <Route path="/bbswrite" element={<Bbswrite />}></Route>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/">Home</Link>
+                  </li>
 
-                <Route path="/bbsdetail/:seq" exact element={<Bbsdetail />}></Route>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/bbslist">게시판</Link>
+                  </li>
 
-                {/* <Route path="/answer/:seq" exact element={}></Route> */}
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/pdslist">자료실</Link>
+                  </li>
 
-                <Route path="/login" element={<Login />} ></Route>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/mypage">마이페이지</Link>
+                  </li>
 
-                <Route path="/regi" element={<Regi />}></Route>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/write">write</Link>
+                  </li>
 
-                <Route path="/bbsanswer/:seq" exact element={<Bbsanswer />}></Route>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/viewer/1">viewer</Link>
+                  </li>
 
-                <Route path="/bbsupdate/:seq" exact element={<Bbsupdate />}></Route>
-
-                <Route path="/write" element={<ToastEditor />}></Route>
-
-                <Route path="/viewer/:seq" exact element={<ToastViewer />}></Route>
-              </Routes>
-
+                </ul>
+              </div>
             </div>
+          </nav>
+
+          <main>
+            <div className="py-4">
+              <div className="container">
+                <Routes>
+                  <Route path="/" element={<Home />}></Route>
+
+                  <Route path="/bbslist" element={<Bbslist />}></Route>
+                  <Route path="/bbslist/:choice/:search" element={<Bbslist />}></Route>
+
+                  <Route path="/bbswrite" element={<Bbswrite />}></Route>
+
+                  <Route path="/bbsdetail/:seq" exact element={<Bbsdetail />}></Route>
+
+                  {/* <Route path="/answer/:seq" exact element={}></Route> */}
+
+                  <Route path="/login" element={<Login />} ></Route>
+
+                  <Route path="/regi" element={<Regi />}></Route>
+
+                  <Route path="/bbsanswer/:seq" exact element={<Bbsanswer />}></Route>
+
+                  <Route path="/bbsupdate/:seq" exact element={<Bbsupdate />}></Route>
+
+                  <Route path="/mypage/*" element={<Mypage />}></Route>
+
+                  <Route path="/write" element={<ToastEditor />}></Route>
+
+                  <Route path="/viewer/:seq" exact element={<ToastViewer />}></Route>
+                </Routes>
+
+              </div>
+            </div>
+          </main>
+
+        </BrowserRouter>
+
+        <footer className="py-4 bg-info text-light">
+          <div className="container text-center">
+            <ul className="nav justify-content-center mb-3">
+              <li className="nav-item">
+                <a href="/" className="nav-link">Top</a>
+              </li>
+
+              <li className="nav-item">
+                <a href="/bbslist" className="nav-link">bbs</a>
+              </li>
+
+            </ul>
+
+            <p>
+              <small>multicampus Copyright</small>
+            </p>
+
           </div>
-        </main>          
-      
-      </BrowserRouter>
+        </footer>
 
-      <footer className="py-4 bg-info text-light">
-        <div className="container text-center">
-          <ul className="nav justify-content-center mb-3">
-            <li className="nav-item">
-              <a href="/" className="nav-link">Top</a>
-            </li>
-
-            <li className="nav-item">
-              <a href="/bbslist" className="nav-link">bbs</a>
-            </li>
-            
-          </ul>
-
-          <p>
-            <small>multicampus Copyright</small>
-          </p>
-
-        </div>
-      </footer>
-      
-    </div>
+      </div>
   );
 }
 
---
+++ src/components/Mypage.js
@@ -0,0 +1,320 @@
+import {Link, Routes, Route, NavLink} from "react-router-dom";
+import "./Mypage.css";
+import {useEffect, useState} from "react";
+import axios from "axios";
+function Nav() {
+    return (
+        <div>
+            <ul>
+                <li><NavLink to="/mypage/profile">프로필</NavLink></li>
+                <li><NavLink to="/mypage/bodycom">체성분</NavLink></li>
+                <li><NavLink to="/mypage/mywrite">내가 쓴 글</NavLink></li>
+                <li><NavLink to="/mypage/setting">설정</NavLink></li>
+            </ul>
+            <Routes>
+                <Route path="profile" element={<Profile/>}></Route>
+                <Route path="bodycom" element={<Bodycom/>}></Route>
+                <Route path="mywrite" element={<Mywrite/>}></Route>
+                <Route path="setting/*" element={<Setting/>}></Route>
+            </Routes>
+
+        </div>
+    )
+}
+
+function Profile() {
+
+    const [member, setMember] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/members/findmember')
+            .then((response) => {
+                setMember(response.data);
+
+                // 데이터 확인용
+                console.log(response.data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, []);
+
+    return (
+        <div>
+            <h2>Profile</h2>
+            <ul>
+                <li>
+                    <hr></hr>
+                    <div>프로필 이미지 : {member.profile}</div>
+                </li>
+                <li>
+                    <div>닉네임 : {member.nickname}</div>
+                </li>
+                <li><Link to="/mypage/follower">팔로워 0(follow)</Link></li>
+                <li><Link to="/mypage/following">팔로잉 0(follow)</Link></li>
+                <li><Link to="/mypage/setting">설정</Link></li>
+                <hr></hr>
+
+                <li><div>나의 헬친</div></li>
+                <li><Link to="/mypage/mate">+나의 헬친을 등록해주세요</Link></li>
+                <li><div>나의 체성분 변화</div></li>
+                <li><Link to="/mypage/bodycom">+나의 체성분을 등록해주세요</Link></li>
+            </ul>
+        </div>
+    )
+}
+
+function Bodycom() {
+
+    const [member, setMember] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/members/findmember')
+            .then((response) => {
+                setMember(response.data);
+
+                // 데이터 확인용
+                console.log(response.data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, []);
+
+    return (
+        <div>
+            <h2>Bodycom</h2>
+            <ul>
+                <li>
+                    <hr></hr>
+                    <div>프로필 이미지 : {member.profile}</div>
+                </li>
+                <li>
+                    <div>닉네임 : {member.nickname}</div>
+                </li>
+                <li><Link to="/mypage/follower">팔로워 0(follow)</Link></li>
+                <li><Link to="/mypage/following">팔로잉 0(follow)</Link></li>
+                <li><Link to="/mypage/setting">설정</Link></li>
+                <hr></hr>
+
+                <li><div>체성분검사 업로드</div></li>
+                <li><Link to="/mypage/upload">Input Form</Link></li>
+                <li><div>업로드 이력</div></li>
+                <li><div>날짜 골격근량 (+-) 체지방량 (+-) (삭제 버튼) </div></li>
+                <li><div>List ...</div></li>
+            </ul>
+        </div>
+    )
+}
+
+function Mywrite() {
+    return (
+        <div>
+            <h2>Mywrite</h2>
+            <ul>
+                <li>
+                    <hr></hr>
+                    <div><h3>내가 쓴 글</h3></div>
+
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+function EditProfile() {
+
+    useEffect(() => {
+        axios
+            .get("http://localhost:3000/members/findmember")
+            .then((response) => {
+                setEmail(response.data.email);
+                setProfile(response.data.profile);
+                setNickname(response.data.nickname);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, []);
+
+    useEffect(() => {
+        axios
+            .get("http://localhost:3000/members/findmemberinfo")
+            .then((response) => {
+                setName(response.data.name);
+                setAge(response.data.age);
+                setGender(response.data.gender);
+                setPhone(response.data.phone);
+                setMbti(response.data.mbti);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, []);
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [nickname, setNickname] = useState('');
+    const [gender, setGender] = useState('');
+    const [age, setAge] = useState('');
+    const [phone, setPhone] = useState('');
+    const [mbti, setMbti] = useState('');
+    const [profile, setProfile] = useState('');
+
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+    }
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    }
+
+    const handleNicknameChange = (event) => {
+        setNickname(event.target.value);
+    }
+
+    const handleGenderChange = (event) => {
+        setGender(event.target.value);
+    }
+
+    const handleAgeChange = (event) => {
+        setAge(event.target.value);
+    }
+
+    const handlePhoneChange = (event) => {
+        setPhone(event.target.value);
+    }
+
+    const handleMbtiChange = (event) => {
+        setMbti(event.target.value);
+    }
+
+    const handleProfileChange = (event) => {
+        setProfile(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        axios.put('http://localhost:3000/members/updatemember', {
+            email: email,
+            nickname: nickname,
+            gender: gender,
+            name: name,
+            age: age,
+            phone: phone,
+            mbti: mbti,
+            profile: profile
+        })
+            .then((response) => {
+                console.log(response.data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                이메일:
+                <input type="email" value={email} onChange={handleEmailChange} />
+            </label>
+            <label>
+                별명:
+                <input type="text" value={nickname} onChange={handleNicknameChange} />
+            </label>
+            <fieldset>
+                <legend>성별:</legend>
+                <label>
+                    <input type="radio" name="gender" value="1" checked={gender === "1"} onChange={handleGenderChange} />
+                    남성
+                </label>
+                <label>
+                    <input type="radio" name="gender" value="0" checked={gender === "0"} onChange={handleGenderChange} />
+                    여성
+                </label>
+            </fieldset>
+            <label>
+                이름:
+                <input type="text" value={name} onChange={handleNameChange} />
+            </label>
+            <label>
+                나이:
+                <input type="text" value={age} onChange={handleAgeChange} />
+            </label>
+            <label>
+                전화번호:
+                <input type="text" value={phone} onChange={handlePhoneChange} />
+            </label>
+            <label>
+                Mbti:
+                <select value={mbti} onChange={handleMbtiChange}>
+                    <option value="ISTJ">ISTJ</option>
+                    <option value="ISFJ">ISFJ</option>
+                    <option value="INFJ">INFJ</option>
+                    <option value="INTJ">INTJ</option>
+                    <option value="ISTP">ISTP</option>
+                    <option value="ISFP">ISFP</option>
+                    <option value="INFP">INFP</option>
+                    <option value="INTP">INTP</option>
+                    <option value="ESTP">ESTP</option>
+                    <option value="ESFP">ESFP</option>
+                    <option value="ENFP">ENFP</option>
+                    <option value="ENTP">ENTP</option>
+                    <option value="ESTJ">ESTJ</option>
+                    <option value="ESFJ">ESFJ</option>
+                    <option value="ENFJ">ENFJ</option>
+                    <option value="ENTJ">ENTJ</option>
+                </select>
+            </label>
+            <label>
+                프로필 이미지:
+                <input type="text" value={profile} onChange={handleProfileChange} />
+            </label>
+            <button type="submit">수정하기</button>
+        </form>
+    );
+}
+
+function Pwdchange() {
+    return (
+        <div>
+            <h2>비밀번호 변경</h2>
+            <ul>
+                <li>
+                    <hr></hr>
+                    <div><h3>Pwdchange</h3></div>
+
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+function Setting() {
+    return (
+        <div>
+            <hr></hr>
+            <ul>
+                <li><Link to="/mypage/setting/editprofile">회원정보 수정</Link></li>
+                <li><Link to="/mypage/setting/pwdchange">비밀번호 변경</Link></li>
+            </ul>
+            <Routes>
+                <Route path="editprofile" element={<EditProfile/>}></Route>
+                <Route path="pwdchange" element={<Pwdchange/>}></Route>
+            </Routes>
+        </div>
+    )
+}
+
+function Mypage() {
+    return (
+        <div>
+            <h1>마이페이지</h1>
+            <Nav></Nav>
+        </div>
+    )
+}
+
+export default Mypage;
