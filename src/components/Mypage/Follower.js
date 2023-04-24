4d6b6993c437b56d05f14d5c08032061b04ad48c
admin <lticdiyk@gmail.com>
2023-04-25 07:10:29 +0900

    마이페이지-인바디

--- src/components/Mypage/Follower.js
+++ src/components/Mypage/Follower.js
@@ -1,11 +1,42 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+
 function Follower() {
+
+    const [followerList, setFollowerList] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/members/follower')
+            .then((response) => {
+                setFollowerList(response.data.followDtoList);
+
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, []);
+
     return (
         <div>
             <hr></hr>
             <h3>팔로워</h3>
-            <p>팔로워가 없습니다</p>
+            {followerList.length > 0 ? (
+                followerList.map((follower) => (
+                    <div key={follower.memberseq}>
+                        <p><img
+                            src={`http://localhost:3000/images/profile/${follower.profile}`}
+                            alt="이미지"
+                            width="30"
+                            height="30"
+                        /> {follower.foltarget}</p>
+                    </div>
+                ))
+            ) : (
+                <p>팔로워가 없습니다</p>
+            )}
             <hr></hr>
         </div>
     )
 }
+
 export default Follower;
