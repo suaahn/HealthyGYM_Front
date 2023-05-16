15b371faf82f2d30574e92256c5086cb4a7e7665
admin <lticdiyk@gmail.com>
2023-05-16 11:50:03 +0900

    마이페이지 feat. 팔로우, 디자인, 게시판 디테일 링크

---
+++ src/components/Mypage/FollowBtn.js
@@ -0,0 +1,83 @@
+import React, {useEffect, useMemo, useState} from "react";
+import axios from "axios";
+
+function FollowBtn({token, foltarget}) {
+
+    const authToken = localStorage.getItem("memberseq");
+    const newtoken = useMemo(() => ({memberseq: authToken}), [authToken]);
+
+    const [userFollow, setUserFollow] = useState(false);
+
+    useEffect(() => {
+
+        const requestBody = {
+            memberseq: newtoken.memberseq,
+            foltarget: foltarget,
+        };
+
+        axios.post("http://localhost:3000/confirm/follow", requestBody, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        }).then((response) => {
+            setUserFollow(response.data);   // follow 여부 세팅
+        });
+    }, [newtoken,foltarget]);
+
+    const handleButtonClick = () => {
+
+        if(userFollow === false){
+
+            const requestBody = {
+                memberseq: token.memberseq,
+                foltarget: foltarget,
+            };
+
+            axios.post("http://localhost:3000/request/follow", requestBody, {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }).then((response) => {
+                setUserFollow(!userFollow);
+            });
+
+        } else {
+
+            const requestBody = {
+                memberseq: token.memberseq,
+                foltarget: foltarget,
+            };
+
+            axios.post("http://localhost:3000/request/unfollow", requestBody, {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }).then((response) => {
+                setUserFollow(!userFollow);
+            });
+        }
+    };
+
+    const buttonClass = userFollow ? "mypage-profilecard-22" : "mypage-profilecard-23";
+
+    return (
+        <div>
+            <div className="mypage-profilecard-12">
+                <div>
+                    <div className={buttonClass} onClick={handleButtonClick}>
+                        {userFollow ? (
+                            <>
+                                <i className="check icon" style={{paddingBottom:'20px',color:'#999'}}></i>
+                                팔로잉
+                            </>
+                        ) : (
+                            "팔로우"
+                        )}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default FollowBtn;
